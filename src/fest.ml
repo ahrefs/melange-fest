a3c56307a0e70288ff29c677cf961b2fbb1568c1
Javier Chávarri <javier.chavarri@gmail.com>
2024-01-09 14:23:20 +0100

    fix binding

--- src/fest.ml
+++ src/fest.ml
@@ -2,6 +2,6 @@ external test : string -> (unit -> unit) -> unit = "test" [@@mel.module "node:te
 
 type assertion
 external expect : assertion = "assert" [@@mel.module]
-external equal : 'a -> 'a -> assertion -> unit = "equal" [@@mel.send]
-external strict_equal : 'a -> 'a -> assertion -> unit = "strictEqual" [@@mel.send]
+external equal : 'a -> 'a -> unit = "equal" [@@mel.send.pipe: assertion]
+external strict_equal : 'a -> 'a -> unit = "strictEqual" [@@mel.send.pipe: assertion]
 let strictEqual = strict_equal
